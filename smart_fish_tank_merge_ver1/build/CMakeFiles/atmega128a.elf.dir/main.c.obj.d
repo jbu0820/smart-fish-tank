9f6d73d5ce9990089725604c5bb6ba316037ffd1
jbu0820 <bungwookjung@gmail.com>
2026-05-13 14:58:28 +0900

    Delete smart_fish_tank_merge_ver1 directory

--- smart_fish_tank_merge_ver1/build/CMakeFiles/atmega128a.elf.dir/main.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/atmega128a.elf.dir/main.c.obj: \
- /home/user15/workspaces/atmega128a/main.c
--- smart_fish_tank_merge_ver1/feeder.h
+++
@@ -1,9 +0,0 @@
-#ifndef FEEDER_H_
-#define FEEDER_H_
-
-#include <stdint.h>
-
-void feeder_init(void);
-uint8_t feeder_update(void);
-
-#endif
